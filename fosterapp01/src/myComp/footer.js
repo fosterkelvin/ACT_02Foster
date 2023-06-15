4405614bc80343da1f37150f356e76624d2fb11f
fosterkelvin <20197992@s.ubaguio.edu>
2023-06-15 10:16:57 +0800

    06/15/23

--- fosterapp01/src/myComp/footer.js
+++ fosterapp01/src/myComp/footer.js
@@ -3,7 +3,6 @@ function Footer(){
         <footer className="bg-dark text-center text-white">
             <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
             © 2023 By: Kelvin Foster
-                <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
             </div>
         </footer>
     );
